8ae5b381eeb45afb755529cfd317b7e01c880633
RichGuilherme <richard.guilhermeAS@hotmail.com>
2024-03-27 13:15:03 -0300

    feat: add novos icons

---
+++ client/src/icons/PlusIcon.tsx
@@ -0,0 +1,10 @@
+
+
+export const PlusIcon = () => {
+  return (
+    
+<svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1.80469 8.92299H8.05469M8.05469 8.92299H14.3047M8.05469 8.92299V2.01413M8.05469 8.92299V15.8318" stroke="#F8F8F8" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+  )
+}
