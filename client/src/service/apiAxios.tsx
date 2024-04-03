2f1ee2dfcf1554adfc9449a0ab5d5420983ddfd2
RichGuilherme <richard.guilhermeAS@hotmail.com>
2024-04-02 23:58:28 -0300

    refactor: troca na port base do servidor

--- client/src/service/apiAxios.tsx
+++ client/src/service/apiAxios.tsx
@@ -2,7 +2,7 @@ import axios from "axios"
 axios.defaults.withCredentials = true
 
 const axiosInstancia = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: "http://localhost:5002",
     headers: {
         "Content-Type": "application/json",
     },
