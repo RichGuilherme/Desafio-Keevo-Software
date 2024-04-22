1c9392b612904652a9272b276576f8a798f5a536
RichGuilherme <richard.guilhermeAS@hotmail.com>
2024-04-22 13:00:20 -0300

    chore: retirada da logo

--- client/src/page/Home/index.tsx
+++ client/src/page/Home/index.tsx
@@ -1,4 +1,3 @@
-import logo from "../../assets/Logo keevo sofware.png"
 import {  TaskSection } from "../../components/TaskSection"
 import styles from "./style.module.css"
 import { HeaderPainelTask } from "../../components/HeaderPainelTask"
@@ -7,11 +6,6 @@ export const Home = () => {
   return (
     <main className={styles.home__main}>
       <header className={styles.home__header}>
-        <img
-          className={styles.home__logoImg}
-          src={logo}
-          alt="Logo da empresa" />
-
         <h1 className={styles.home__title}>
           Lista De Terefas
         </h1>
